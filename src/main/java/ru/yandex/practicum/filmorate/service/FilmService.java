ffae845eae8c0a9a2766e7f16ff31c0b9b702ba9
Sergey Drozdov <SSDrozdov@alfabank.ru>
2024-08-13 01:16:00 +0300

    + first commit sprint 12 (fix checkstyle)

--- src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
@@ -2,7 +2,6 @@ package ru.yandex.practicum.filmorate.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import ru.yandex.practicum.filmorate.exception.NotFoundException;
 import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.model.User;
 import ru.yandex.practicum.filmorate.storage.LikeDbStorage;
@@ -12,7 +11,6 @@ import ru.yandex.practicum.filmorate.storage.impl.UserDbStorage;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
-import java.util.Set;
 
 @Service
 @RequiredArgsConstructor
